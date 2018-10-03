cf2bdde13443421385495a4c1e315159301b8e95
edusito <43457188+edusito@users.noreply.github.com>
2018-10-03 02:46:51 -0500

    https://github.com/edusito/UppLaboratorio3.git

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,11 +84,10 @@
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laracasts.com">Quienes somos</a>
+                    <a href="https://laravel-news.com">MISION</a>
+                    <a href="https://nova.laravel.com">VISION</a>
+                    <a href="https://github.com/laravel/laravel">Nuestro GitHub</a>
                 </div>
             </div>
         </div>
